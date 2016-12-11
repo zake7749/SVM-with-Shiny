fb3dec12f529a7516a0c7a4e077363a8d50ed1fd
zake7749 <zake7749@gmail.com>
2016-12-11 15:50:44 +0800

    clean.

--- deploy.R
+++
@@ -1,6 +0,0 @@
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='zake7749',
-                          token='DDEE7AE4DF6260DBF0DE01C37C166DD6',
-                          secret='<SECRET>')
-rsconnect::deployApp('.')
